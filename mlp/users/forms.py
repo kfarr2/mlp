4c41c8c348de7fde862cfa745259f792c4995c81
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-07 12:18:28 -0700

    created login form; added views for users

--- mlp/users/forms.py
+++ mlp/users/forms.py
@@ -1,10 +1,78 @@
+import os
+import sys
 from django import forms
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model, authenticate
+from django.core.validators import validate_email
 from .models import User
 
 class UserForm(forms.ModelForm):
+
     class Meta:
         model = User
+        fields = (
+            'email',                
+            'first_name',
+            'last_name',
+        )
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user")
+        super(UserForm, self).__init__(*args, **kwargs)
+
+        if self.instance.pk is None:
+            self.fields['password'] = forms.CharField()
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+
+        if self.in_create_mode():
+            email = cleaned_data.get("email", None)
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist as e:
+                user = None
+
+    def in_create_mode(self):
+        return self.instance.pk is None
 
     def save(self, *args, **kwargs):
-        self.instance.set_password(self.cleaned_data.pop("password"))
+        if self.in_create_mode():
+            self.instance.set_password(self.cleaned_data.pop("password"))
+        
         return super(UserForm, self).save(*args, **kwargs)
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    next = forms.CharField(widget=forms.HiddenInput, required=False)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user_model = get_user_model()
+        try:
+            user_model.objects.get(email=email)
+        except user_model.DoesNotExist:
+            raise forms.ValidationError("Invalid Email")
+
+        return email
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        user_model = get_user_model()
+
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                if not user.is_active:
+                    raise forms.ValidationError("Your account is not active")
+                else:
+                    cleaned_data['user'] = user
+
+            else:
+                raise forms.ValidationError("Incorrect Password")
+
+        return cleaned_data
--- mlp/users/views.py
+++ mlp/users/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+import os
+import sys
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import User
+from .forms import UserForm
 
-# Create your views here.
+@login_required
+def home(request):
+    """
+    User-home. Either redirect to the workflow or admin page
+    """
+    if request.user.is_staff:
+        return HttpResponseRedirect(reverse("users-admin"))
+    else:
+        return HttpResponseRedirect(reverse("users-workflow"))
+
+@login_required
+def admin(request):
+    """
+    Admin page
+    """
+    return render(request, "users/admin.html", {
+        
+    })
+
+@login_required
+def workflow(request):
+    """
+    Workflow page
+    """
+    return render(request, "users/workflow.html", {
+        
+    })
+
+@login_required
+def edit(request, user_id):
+    """
+    Edit
+    """
+    return _edit(request, user_id)
+
+@login_required
+def _edit(request, user_id):
+    """Creates a new user if user_id=None, otherwise, it will edit the user"""
+    if user_id is None:
+        user = None
+    else:
+        user = get_object_or_404(User, pk=user_id)
+
+    if request.POST:
+        form = UserForm(request.POST, instance=user, user=request.user)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Updated")
+            return HttpResponseRedirect(reverse("users-list"))
+    else:
+        form = UserForm(instance=user, user=request.user)
+
+    return render(request, "users/edit.html", {
+        "form": form,
+    })
