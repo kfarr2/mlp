8a7be371aab64a0597c48d9103e63aa699419d79
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-07 12:17:35 -0700

    added urls for users

--- mlp/urls.py
+++ mlp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
-from .views import home
+
+from mlp.home import views as home
+from mlp.users import views as users
 
 admin.autodiscover()
 
@@ -12,8 +14,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'mlp.views.home', name='home'),
+    url(r'^$', home.home, name='home'),
 
+    # users
+    url(r'^users/home/$', users.home, name='users-home'),
+    url(r'^users/admin/$', users.admin, name='users-admin'),
+    url(r'^users/workflow/$', users.workflow, name='users-workflow'),
+    url(r'^users/edit/(?P<user_id>\d+)/?$', users.edit, name='users-edit'),
 
     url(r'', include('django.contrib.auth.urls')),
     url(r'^cloak/', include('cloak.urls'))
