b28612ed23bad83d3bd617496ae5600d641600d7
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-12 14:36:53 -0700

    added perms.py file

---
+++ mlp/classes/perms.py
@@ -0,0 +1,31 @@
+import os
+from permissions import permission
+from django.db.models import Q
+from mlp.users.models import User
+from .models import Class, Roster
+
+@permission(model=Class)
+def can_list_class(user, _class):
+    return user.is_staff or can_list_all_files(user)
+
+@permission
+def can_list_all_classes(user):
+    return user.is_staff or user.is_authenticated
+
+@permission(model=Class)
+def can_edit_class(user, _class):
+    roster = Roster.objects.filter(user=user, _class=_class, role=4)
+    if user.is_staff or roster:
+        return True
+    return False
+
+@permission
+def can_create_class(user):
+    roster = Roster.objects.filter(user=user, role=4)
+    if user.is_staff or roster:
+        return True
+    return False
+
+@permission(model=Class)
+def can_enroll_students(user, _class):
+    return can_edit_class(user, _class) 
---
+++ mlp/files/perms.py
@@ -0,0 +1,35 @@
+import os
+from permissions import permission
+from django.db.models import Q
+from mlp.users.models import User
+from mlp.classes.models import Class, Roster
+from .models import File
+
+@permission
+def can_upload_file(user):
+    roster = Roster.objects.filter(user=user, role=4)
+    if user.is_staff or roster:
+        return True
+
+@permission(model=File)
+def can_edit_file(user, file):
+    if user.is_staff:
+        return True
+    else:
+        return file.uploaded_by == user
+
+@permission
+def can_list_file(user):
+    return can_list_all_files(user) or user.is_staff
+
+@permission
+def can_list_all_files(user):
+    return user.is_authenticated
+
+@permission(model=File)
+def can_view_file(user, file):
+    return can_list_all_files(user) or can_edit_file(user, file)
+
+@permission(model=File)
+def can_download_file(user, file):
+    return can_edit_file(user, file)
