5b88cc7ec176183f434b01697e9f17b5a038bbb8
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-14 16:14:08 -0700

    added more specific permissions

--- mlp/users/perms.py
+++ mlp/users/perms.py
@@ -8,6 +8,10 @@ from .models import User
 def can_view_users(user):
     return user.is_staff
 
+@permission(model=User)
+def can_view_user_detail(user, other_user):
+    return user.is_staff or user == other_user
+
 @permission
 def has_admin_access(user):
     return user.is_staff
@@ -15,3 +19,11 @@ def has_admin_access(user):
 @permission
 def can_create_users(user):
     return has_admin_access(user)
+
+@permission(model=User)
+def can_edit_user(user, other_user):
+    return can_edit_all_users(user) or user == other_user
+
+@permission
+def can_edit_all_users(user):
+    return user.is_staff
