ed72255b84d012ea9ca4d72ed14f58ba53f0abb9
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-20 12:10:38 -0700

    changed permissions around

--- mlp/users/perms.py
+++ mlp/users/perms.py
@@ -7,21 +7,21 @@ from .models import User
 
 @permission
 def can_view_users(user):
-    return user.is_staff
+    return has_admin_access(user)
 
 @permission(model=User)
 def can_view_user_detail(user, other_user):
-    roster = Roster.objects.filter(user=user, role=UserRole.ADMIN)
-    for ros in roster:
-	    other_roster = Roster.objects.filter(user=other_user, _class=ros._class)
-	    if other_roster.exists():
-		return True
-    return user.is_staff or user == other_user
+    roster = Roster.objects.filter(user=user, role=UserRole.ADMIN).values('_class')
+    other_roster = Roster.objects.filter(user=other_user, _class=roster)
+    return user.is_staff or user == other_user or other_roster.exists()
 
 @permission
 def has_admin_access(user):
-    roster = Roster.objects.filter(user=user, role=UserRole.ADMIN)
-    return user.is_staff or roster.exists()
+    if user.is_anonymous():
+        return False
+    else:
+        roster = Roster.objects.filter(user=user, role=UserRole.ADMIN)
+        return roster.exists() or user.is_staff
 
 @permission
 def can_create_users(user):
@@ -33,7 +33,7 @@ def can_edit_user(user, other_user):
     for ros in roster:
 	    other_roster = Roster.objects.filter(user=other_user, _class=ros._class)
 	    if other_roster.exists():
-		return True
+		    return True
     return can_edit_all_users(user) or user == other_user
 
 @permission
