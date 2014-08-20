d7fbe7101006880f727857ebc4d29cd640714a9d
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-20 12:05:08 -0700

    changed permissions

--- mlp/files/perms.py
+++ mlp/files/perms.py
@@ -4,20 +4,16 @@ from django.db.models import Q
 from mlp.users.models import User
 from mlp.classes.enums import UserRole
 from mlp.classes.models import Class, Roster, ClassFile
+from mlp.users.perms import has_admin_access
 from .models import File
 
 @permission
 def can_upload_file(user):
-    roster = Roster.objects.filter(user=user, role=UserRole.ADMIN)
-    if user.is_staff or roster:
-        return True
+    return user.is_authenticated 
 
 @permission(model=File)
 def can_edit_file(user, file):
-    if user.is_staff:
-        return True
-    else:
-        return file.uploaded_by == user
+    return has_admin_access(user) or file.uploaded_by == user
 
 @permission(model=File)
 def can_list_file(user, file):
@@ -25,7 +21,7 @@ def can_list_file(user, file):
 
 @permission
 def can_list_all_files(user):
-    return user.is_staff
+    return has_admin_access(user)
 
 @permission(model=File)
 def can_view_file(user, file):
