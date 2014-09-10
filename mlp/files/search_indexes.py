6356aba927babea17700ad45a67d1c6a9db2a1e2
konstantin farrell <konstantinfarrell@gmail.com>
2014-09-10 13:36:32 -0700

    Added handling in search indexes for multiple files

--- mlp/files/search_indexes.py
+++ mlp/files/search_indexes.py
@@ -28,9 +28,9 @@ class FileIndex(Indexable):
         }
 
     def prepare(self, obj):
-        courses = ClassFile.objects.filter(file=obj).values('_class')
+        courses = ClassFile.objects.filter(file=obj).first()
         if courses:
-            course = Class.objects.get(class_id__in=courses).name
+            course = Class.objects.get(class_id__in=courses._class.class_id).name
         else:
             course = None
 
