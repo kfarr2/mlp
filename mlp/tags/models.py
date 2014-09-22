80cea52827fd37d30d48783d2d8cd8d8ea651e12
konstantin farrell <konstantinfarrell@gmail.com>
2014-09-22 09:29:08 -0700

    Fixed bug: attribute error when adding associated files

--- mlp/tags/models.py
+++ mlp/tags/models.py
@@ -13,7 +13,7 @@ class TaggableManager(models.Manager):
         classname
         """
         # we assume the many2many table has a field with this particular name
-        field_name = taggable_object._group__.__name__.lower()
+        field_name = taggable_object.__class__.__name__.lower()
         tags = set(tags or [])
         queryset = self.filter(**{
             field_name: taggable_object,
